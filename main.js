6a5e44963dedf215f9b30e26e7a88605ba27ba7d
Lizbeth Cortés León <60724433+lizbeth-cortes@users.noreply.github.com>
2024-06-12 22:36:10 -0600

    Página actualizada

--- main.js
+++ main.js
@@ -22,10 +22,44 @@ let frase = new Typewriter(phrase, {
 frase
   .pauseFor(8000)
   .typeString(
-    "\"No es valiente aquel que no tiene miedo,\n" + 
-    "sino aquel que sabe conquistarlo\"\n" + 
-    "- Nelson Mandela." 
+    '"No es valiente aquel que no tiene miedo,\n' +
+      'sino aquel que sabe conquistarlo"\n' +
+      "- Nelson Mandela."
   )
   .pauseFor(1100)
   .deleteChars(10)
   .start();
+
+const fondoAzul = (elemento) => {
+  const badges = elemento.querySelectorAll(".badge");
+  const titulos = elemento.querySelectorAll(".experiencia-titulo");
+  const icons = elemento.querySelectorAll("i");
+  badges.forEach((badge) => {
+    badge.style.backgroundColor = "#587AC8";
+  });
+
+  titulos.forEach((element) => {
+    element.style.color = "#8EA4D2";
+  });
+
+  icons.forEach((element) => {
+    element.style.color = "#8EA4D2";
+  });
+};
+
+const fondoNormal = (elemento) => {
+  const badges = elemento.querySelectorAll(".badge");
+  const titulos = elemento.querySelectorAll(".experiencia-titulo");
+  const icons = elemento.querySelectorAll("i");
+  badges.forEach((badge) => {
+    badge.style.backgroundColor = "#B8336A";
+  });
+
+  titulos.forEach((element) => {
+    element.style.color = "#B8336A";
+  });
+
+  icons.forEach((element) => {
+    element.style.color = "#B8336A";
+  });
+};
